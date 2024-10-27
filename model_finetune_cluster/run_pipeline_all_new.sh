1fe7f4e84432b043cd93d17c48912d4382dd6fbb
jlpang <jlpang863@gmail.com>
2024-10-27 16:31:23 -0700

    update

--- model_finetune_cluster/run_pipeline_all_new.sh
+++ model_finetune_cluster/run_pipeline_all_new.sh
@@ -58,226 +58,217 @@ cluster_root_path="output" ## . for local
 
 mkdir -p $cluster_root_path
 
-for base_model in "${!base_models[@]}"
-do
-    IFS=' ' read -r -a params <<< "${base_models[$base_model]}"
-    TOTAL_BATCH_SIZE=${params[0]}
-    BATCH_SIZE_PER_GPU=${params[1]}
-    max_seq_length=${params[2]}
+# for base_model in "${!base_models[@]}"
+# do
+#     IFS=' ' read -r -a params <<< "${base_models[$base_model]}"
+#     TOTAL_BATCH_SIZE=${params[0]}
+#     BATCH_SIZE_PER_GPU=${params[1]}
+#     max_seq_length=${params[2]}
 
 
-    for train_dataset_name in "${TRAIN_DATASET_LIST[@]}"
-    do
+#     for train_dataset_name in "${TRAIN_DATASET_LIST[@]}"
+#     do
 
-        echo "###### Processing training dataset :: ${train_dataset_name}"
-
-
-        for data_type in "${data_types[@]}"
-        do
-            echo "###### Processing data type:: ${data_type}"
-
-            if [[ $data_type == "base" ]]; then
-                echo "Skipping base model finetune"
-                continue     
-            fi
-
-            mkdir -p $cluster_root_path/models/
-
-            # train_data="score_curation/data/${labeling_model}/${dataset_name}/${data_type}_dataset.json"
-            echo "Processing data type: $data_type"
-
-            train_data="new_train_data/${labeling_model}/${train_dataset_name}/${data_type}_dataset.json"
-
-            echo "train data path: $train_data"
-
-            GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
-            echo "Training ${base_model} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
-            echo "Training data path: ${train_data}"
-
-            ### Lora training                 
-            # --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
-            # --use_qlora \
-
-            accelerate launch \
-                --mixed_precision bf16 \
-                --num_machines 1 \
-                --num_processes $NUM_GPUS \
-                open_instruct/finetune.py \
-                --model_name_or_path $base_model \
-                --gradient_checkpointing \
-                --use_lora \
-                --use_qlora \
-                --lora_rank 64 \
-                --lora_alpha 16 \
-                --lora_dropout 0.1 \
-                --tokenizer_name $base_model \
-                --use_slow_tokenizer \
-                --train_file $train_data \
-                --max_seq_length $max_seq_length \
-                --preprocessing_num_workers 16 \
-                --checkpointing_steps epoch \
-                --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
-                --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
-                --learning_rate 1e-4 \
-                --lr_scheduler_type linear \
-                --warmup_ratio 0.03 \
-                --weight_decay 0. \
-                --num_train_epochs 5 \
-                --output_dir $cluster_root_path/models/${labeling_model}/${train_dataset_name}/${base_model}/lora_${data_type}/ \
-                --with_tracking \
-                --report_to tensorboard \
-                --logging_steps 1
-
-            python open_instruct/merge_lora.py \
-                --base_model_name_or_path $base_model \
-                --lora_model_name_or_path $cluster_root_path/models/${labeling_model}/${train_dataset_name}/${base_model}/lora_${data_type}/ \
-                --output_dir $cluster_root_path/models/${labeling_model}/${train_dataset_name}/${base_model}/lora_merged_${data_type}/ \
-                --save_tokenizer \
-                --qlora 
+#         echo "###### Processing training dataset :: ${train_dataset_name}"
 
 
-            sleep 10s
-
-            rm -rf $cluster_root_path/models/${labeling_model}/${train_dataset_name}/${base_model}/lora_${data_type}
-
-        done
-    done
-done
-
-
-wait
-sleep 10s
-
-
-# AZURE_STORAGE_CONTAINER_URL="https://afminternshipuksouth.blob.core.windows.net/jinlong/finetune_result/?sp=racwdlmeop&st=2024-08-24T00:58:39Z&se=2025-04-03T08:58:39Z&sv=2022-11-02&sr=c&sig=rbf41XiVlLJw76zeillA%2FRMAjgGMo2lQHO3m3RW5Ho8%3D"
-# azcopy copy "$cluster_root_path/*" "$AZURE_STORAGE_CONTAINER_URL" --recursive
-
-
-# echo "starting evaluating finetuned models..."
-
-
-
-# # # ############################################################
-# # # ######## ####  finetuned model  evaluation ######## #### 
-# # # ###########################################################
-# for base_model in "${!base_models[@]}"; do
-
-#     for train_dataset_name in "${TRAIN_DATASET_LIST[@]}"; do
-
-#         for data_type in "${data_types[@]}"; do
-
-#             model_name_or_path=$cluster_root_path/models/${labeling_model}/${train_dataset_name}/${base_model}/lora_merged_${data_type}
-
-#             if [[ $data_type == "base" ]]; then
-#                 echo "base model evaluation"
-#                 model_name_or_path=$base_model
-#             fi
-
+#         for data_type in "${data_types[@]}"
+#         do
 #             echo "###### Processing data type:: ${data_type}"
 
-#             ####MMLU: factual knowledge
-#             #### ./scripts/eval-new/mmlu.sh "$train_dataset_name" "$labeling_model" "$base_model" "$models" "$save_dirs" "$cuda_devices"
-            
-#             eval_dataset_name='mmlu'
-#             local_save_dir=${cluster_root_path}/results/${labeling_model}/${train_dataset_name}/${eval_dataset_name}/${base_model}/$data_type
+#             if [[ $data_type == "base" ]]; then
+#                 echo "Skipping base model finetune"
+#                 continue     
+#             fi
 
-#             CUDA_VISIBLE_DEVICES=0 python -m eval.mmlu.run_eval \
-#             --ntrain 0 \
-#             --data_dir raw_data/eval/mmlu \
-#             --save_dir ${local_save_dir} \
-#             --model_name_or_path $model_name_or_path \
-#             --tokenizer_name_or_path  $model_name_or_path \
-#             --eval_batch_size 8  &
+#             mkdir -p $cluster_root_path/models/
 
-#             # # ### reasoning
-#             # # ./scripts/eval-new/gsm.sh "$train_dataset_name" "$labeling_model" "$base_model" "$model_declaration" "$save_dirs_declaration" "$cuda_devices_declaration"
-            
-#             eval_dataset_name='gsm'
-#             local_save_dir=${cluster_root_path}/results/${labeling_model}/${train_dataset_name}/${eval_dataset_name}/${base_model}/$data_type
+#             # train_data="score_curation/data/${labeling_model}/${dataset_name}/${data_type}_dataset.json"
+#             echo "Processing data type: $data_type"
 
-#             CUDA_VISIBLE_DEVICES=2,5 python -m eval.gsm.run_eval \
-#                 --data_dir raw_data/eval/gsm/ \
-#                 --max_num_examples 200 \
-#                 --save_dir ${local_save_dir} \
-#                 --model_name_or_path $model_name_or_path \
-#                 --tokenizer_name_or_path $model_name_or_path \
-#                 --n_shot 8 \
-#                 --use_vllm &
+#             train_data="new_train_data/${labeling_model}/${train_dataset_name}/${data_type}_dataset.json"
 
-#             # BBH: 
-#             ## ./scripts/eval-new/bbh.sh "$train_dataset_name" "$labeling_model" "$base_model"  "${!models[@]}" "${!save_dirs[@]}" "$cuda_devices"
-        
-#             eval_dataset_name='bbh'
-#             local_save_dir=${cluster_root_path}/results/${labeling_model}/${train_dataset_name}/${eval_dataset_name}/${base_model}/$data_type
+#             echo "train data path: $train_data"
 
-#             CUDA_VISIBLE_DEVICES=1 python -m eval.bbh.run_eval \
-#                 --data_dir raw_data/eval/bbh \
-#                 --save_dir ${local_save_dir} \
-#                 --model_name_or_path $model_name_or_path  \
-#                 --tokenizer_name_or_path $model_name_or_path \
-#                 --max_num_examples_per_task 40 \
-#                 --use_vllm &
+#             GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
+#             echo "Training ${base_model} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
+#             echo "Training data path: ${train_data}"
 
-#             # # ### truthfulness
-#             # # ./scripts/eval-new/truthfulqa.sh "$train_dataset_name" "$labeling_model" "$base_model" "$model_declaration" "$save_dirs_declaration" "$cuda_devices_declaration"
-            
-#             eval_dataset_name='truthfulqa'
-#             local_save_dir=${cluster_root_path}/results/${labeling_model}/${train_dataset_name}/${eval_dataset_name}/${base_model}/$data_type
+#             ### Lora training                 
+#             # 
+#             # --use_qlora \ 
+#             # --use_deepspeed \
+#             # --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
+#             accelerate launch \
+#                 --mixed_precision bf16 \
+#                 --num_machines 1 \
+#                 --num_processes $NUM_GPUS \
+#                 --fsdp_sharding_strategy FULL_SHARD \
+#                 --fsdp_offload_params true \
+#                 open_instruct/finetune.py \
+#                 --model_name_or_path $base_model \
+#                 --gradient_checkpointing \
+#                 --use_lora \
+#                 --lora_rank 64 \
+#                 --lora_alpha 16 \
+#                 --lora_dropout 0.1 \
+#                 --tokenizer_name $base_model \
+#                 --use_slow_tokenizer \
+#                 --train_file $train_data \
+#                 --max_seq_length $max_seq_length \
+#                 --preprocessing_num_workers 16 \
+#                 --checkpointing_steps epoch \
+#                 --per_device_train_batch_size $BATCH_SIZE_PER_GPU \
+#                 --gradient_accumulation_steps $GRADIENT_ACC_STEPS \
+#                 --learning_rate 1e-4 \
+#                 --lr_scheduler_type linear \
+#                 --warmup_ratio 0.03 \
+#                 --weight_decay 0. \
+#                 --num_train_epochs 5 \
+#                 --output_dir $cluster_root_path/models/${labeling_model}/${train_dataset_name}/${base_model}/lora_${data_type}/ \
+#                 --with_tracking \
+#                 --report_to tensorboard \
+#                 --logging_steps 1 \
+#                 # --use_qlora 
 
-#             CUDA_VISIBLE_DEVICES=3 python -m eval.truthfulqa.run_eval \
-#                 --data_dir raw_data/eval/truthfulqa \
-#                 --save_dir ${local_save_dir} \
-#                 --model_name_or_path $model_name_or_path \
-#                 --tokenizer_name_or_path $model_name_or_path \
-#                 --metrics truth info mc \
-#                 --preset qa \
-#                 --hf_truth_model_name_or_path allenai/truthfulqa-truth-judge-llama2-7B \
-#                 --hf_info_model_name_or_path allenai/truthfulqa-info-judge-llama2-7B \
-#                 --eval_batch_size 20 \
-#                 --load_in_8bit &
+#             python open_instruct/merge_lora.py \
+#                 --base_model_name_or_path $base_model \
+#                 --lora_model_name_or_path $cluster_root_path/models/${labeling_model}/${train_dataset_name}/${base_model}/lora_${data_type}/ \
+#                 --output_dir $cluster_root_path/models/${labeling_model}/${train_dataset_name}/${base_model}/lora_merged_${data_type}/ \
+#                 --save_tokenizer \
+#                 # --qlora
 
+#             sleep 10s
 
-#             # # # # ### multilinguality
-#             # # # # ./scripts/eval-new/tydiqa.sh "$train_dataset_name" "$labeling_model" "$base_model" "$model_declaration" "$save_dirs_declaration" "$cuda_devices_declaration"
-            
-#             eval_dataset_name='tydiqa'
-#             local_save_dir=${cluster_root_path}/results/${labeling_model}/${train_dataset_name}/${eval_dataset_name}/${base_model}/$data_type
-
-#             CUDA_VISIBLE_DEVICES=4 python -m eval.tydiqa.run_eval \
-#                 --data_dir raw_data/eval/tydiqa/ \
-#                 --n_shot 1 \
-#                 --max_num_examples_per_lang 100 \
-#                 --max_context_length 512 \
-#                 --save_dir ${local_save_dir} \
-#                 --model_name_or_path $model_name_or_path \
-#                 --tokenizer_name_or_path $model_name_or_path \
-#                 --eval_batch_size 20 \
-#                 --load_in_8bit &
-
-#             wait
+#             rm -rf $cluster_root_path/models/${labeling_model}/${train_dataset_name}/${base_model}/lora_${data_type}
 
 #         done
-
 #     done
 # done
 
+
+
 # sleep 10s
 
-# for base_model in "${!base_models[@]}"; do
-#     for train_dataset_name in "${TRAIN_DATASET_LIST[@]}"; do
+# ############################################################
+# ######## ####  finetuned model  evaluation ######## #### 
+# ###########################################################
 
-#         for data_type in "${data_types[@]}"; do        
-#         echo "###### Processing Base model :: ${labeling_model}"
-#         echo "###### Processing Labeling model :: ${base_model}"
-#         echo "###### Processing training dataset :: ${train_dataset_name}"
-#         echo "###### Processing data type :: ${data_type}"
+for base_model in "${!base_models[@]}"; do
 
-#         python3 read_results.py --root_result_path "${cluster_root_path}/results" --train_dataset $train_dataset_name --base_model $base_model --labeling_model $labeling_model --baseline_tag $data_type
+    for train_dataset_name in "${TRAIN_DATASET_LIST[@]}"; do
 
-#         done
+        for data_type in "${data_types[@]}"; do
 
-#     done
-# done 
+            model_name_or_path=$cluster_root_path/models/${labeling_model}/${train_dataset_name}/${base_model}/lora_merged_${data_type}
 
-# AZURE_STORAGE_CONTAINER_URL="https://afminternshipuksouth.blob.core.windows.net/jinlong/evaluation_result/results/?sp=racwdlmeop&st=2024-08-24T00:58:39Z&se=2025-04-03T08:58:39Z&sv=2022-11-02&sr=c&sig=rbf41XiVlLJw76zeillA%2FRMAjgGMo2lQHO3m3RW5Ho8%3D"
-# azcopy copy "$cluster_root_path/results/*" "$AZURE_STORAGE_CONTAINER_URL" --recursive
+            if [[ $data_type == "base" ]]; then
+                echo "base model evaluation"
+                model_name_or_path=$base_model
+            fi
+
+            echo "###### Processing data type:: ${data_type}"
+
+            ####MMLU: factual knowledge
+            #### ./scripts/eval-new/mmlu.sh "$train_dataset_name" "$labeling_model" "$base_model" "$models" "$save_dirs" "$cuda_devices"
+            
+            # eval_dataset_name='mmlu'
+            # local_save_dir=${cluster_root_path}/results/${labeling_model}/${train_dataset_name}/${eval_dataset_name}/${base_model}/$data_type
+
+            # CUDA_VISIBLE_DEVICES=0 python -m eval.mmlu.run_eval \
+            # --ntrain 0 \
+            # --data_dir raw_data/eval/mmlu \
+            # --save_dir ${local_save_dir} \
+            # --model_name_or_path $model_name_or_path \
+            # --tokenizer_name_or_path  $model_name_or_path \
+            # --eval_batch_size 8  &
+
+            # # # ### reasoning
+            # # # ./scripts/eval-new/gsm.sh "$train_dataset_name" "$labeling_model" "$base_model" "$model_declaration" "$save_dirs_declaration" "$cuda_devices_declaration"
+            
+            # eval_dataset_name='gsm'
+            # local_save_dir=${cluster_root_path}/results/${labeling_model}/${train_dataset_name}/${eval_dataset_name}/${base_model}/$data_type
+
+            # CUDA_VISIBLE_DEVICES=2,5 python -m eval.gsm.run_eval \
+            #     --data_dir raw_data/eval/gsm/ \
+            #     --max_num_examples 200 \
+            #     --save_dir ${local_save_dir} \
+            #     --model_name_or_path $model_name_or_path \
+            #     --tokenizer_name_or_path $model_name_or_path \
+            #     --n_shot 8 \
+            #     --use_vllm &
+
+            # # BBH: 
+            # ## ./scripts/eval-new/bbh.sh "$train_dataset_name" "$labeling_model" "$base_model"  "${!models[@]}" "${!save_dirs[@]}" "$cuda_devices"
+        
+            # eval_dataset_name='bbh'
+            # local_save_dir=${cluster_root_path}/results/${labeling_model}/${train_dataset_name}/${eval_dataset_name}/${base_model}/$data_type
+
+            # CUDA_VISIBLE_DEVICES=1 python -m eval.bbh.run_eval \
+            #     --data_dir raw_data/eval/bbh \
+            #     --save_dir ${local_save_dir} \
+            #     --model_name_or_path $model_name_or_path  \
+            #     --tokenizer_name_or_path $model_name_or_path \
+            #     --max_num_examples_per_task 40 \
+            #     --use_vllm &
+
+            # # ### truthfulness
+            # # ./scripts/eval-new/truthfulqa.sh "$train_dataset_name" "$labeling_model" "$base_model" "$model_declaration" "$save_dirs_declaration" "$cuda_devices_declaration"
+            
+            eval_dataset_name='truthfulqa'
+            local_save_dir=${cluster_root_path}/results/${labeling_model}/${train_dataset_name}/${eval_dataset_name}/${base_model}/$data_type
+
+            CUDA_VISIBLE_DEVICES=3 python -m eval.truthfulqa.run_eval \
+                --data_dir raw_data/eval/truthfulqa \
+                --save_dir ${local_save_dir} \
+                --model_name_or_path $model_name_or_path \
+                --tokenizer_name_or_path $model_name_or_path \
+                --metrics truth info mc \
+                --preset qa \
+                --hf_truth_model_name_or_path allenai/truthfulqa-truth-judge-llama2-7B \
+                --hf_info_model_name_or_path allenai/truthfulqa-info-judge-llama2-7B \
+                --eval_batch_size 20 
+                # --load_in_8bit &
+
+
+            # # # # ### multilinguality
+            # # # # ./scripts/eval-new/tydiqa.sh "$train_dataset_name" "$labeling_model" "$base_model" "$model_declaration" "$save_dirs_declaration" "$cuda_devices_declaration"
+            
+            # eval_dataset_name='tydiqa'
+            # local_save_dir=${cluster_root_path}/results/${labeling_model}/${train_dataset_name}/${eval_dataset_name}/${base_model}/$data_type
+
+            # CUDA_VISIBLE_DEVICES=4 python -m eval.tydiqa.run_eval \
+            #     --data_dir raw_data/eval/tydiqa/ \
+            #     --n_shot 1 \
+            #     --max_num_examples_per_lang 100 \
+            #     --max_context_length 512 \
+            #     --save_dir ${local_save_dir} \
+            #     --model_name_or_path $model_name_or_path \
+            #     --tokenizer_name_or_path $model_name_or_path \
+            #     --eval_batch_size 20 \
+            #     --load_in_8bit &
+
+            wait
+
+        done
+
+    done
+done
+
+sleep 10s
+
+for base_model in "${!base_models[@]}"; do
+    for train_dataset_name in "${TRAIN_DATASET_LIST[@]}"; do
+
+        for data_type in "${data_types[@]}"; do        
+        echo "###### Processing Base model :: ${labeling_model}"
+        echo "###### Processing Labeling model :: ${base_model}"
+        echo "###### Processing training dataset :: ${train_dataset_name}"
+        echo "###### Processing data type :: ${data_type}"
+
+        python3 read_results.py --root_result_path "${cluster_root_path}/results" --train_dataset $train_dataset_name --base_model $base_model --labeling_model $labeling_model --baseline_tag $data_type
+
+        done
+
+    done
+done 
--- model_finetune_cluster/scripts/prepare_eval_data.sh
+++ model_finetune_cluster/scripts/prepare_eval_data.sh
@@ -24,42 +24,42 @@ wget -P data/eval/tydiqa/ https://storage.googleapis.com/tydiqa/v1.1/tydiqa-gold
 # GSM dataset
 wget -P data/eval/gsm/ https://github.com/openai/grade-school-math/raw/master/grade_school_math/data/test.jsonl
 
-# MATH dataset
-mkdir -p data/eval/MATH
-wget -P data/eval/MATH/ https://raw.githubusercontent.com/deepseek-ai/DeepSeek-Math/main/evaluation/datasets/math/test.jsonl
+# # MATH dataset
+# mkdir -p data/eval/MATH
+# wget -P data/eval/MATH/ https://raw.githubusercontent.com/deepseek-ai/DeepSeek-Math/main/evaluation/datasets/math/test.jsonl
 
-# Codex HumanEval
-wget -P data/eval/codex_humaneval https://github.com/openai/human-eval/raw/master/data/HumanEval.jsonl.gz
-wget -P data/eval/codex_humaneval https://huggingface.co/datasets/bigcode/humanevalpack/raw/main/data/python/data/humanevalpack.jsonl
+# # Codex HumanEval
+# wget -P data/eval/codex_humaneval https://github.com/openai/human-eval/raw/master/data/HumanEval.jsonl.gz
+# wget -P data/eval/codex_humaneval https://huggingface.co/datasets/bigcode/humanevalpack/raw/main/data/python/data/humanevalpack.jsonl
 
-# HumanEval+
-wget -P data/eval/codex_humaneval https://github.com/evalplus/humanevalplus_release/releases/download/v0.1.9/HumanEvalPlus-OriginFmt.jsonl.gz
+# # HumanEval+
+# wget -P data/eval/codex_humaneval https://github.com/evalplus/humanevalplus_release/releases/download/v0.1.9/HumanEvalPlus-OriginFmt.jsonl.gz
 
-# Alpaca Farm reference
-wget -P data/eval/alpaca_farm https://huggingface.co/datasets/hamishivi/alpaca-farm-davinci-003-2048-token/resolve/main/davinci_003_outputs.json
+# # Alpaca Farm reference
+# wget -P data/eval/alpaca_farm https://huggingface.co/datasets/hamishivi/alpaca-farm-davinci-003-2048-token/resolve/main/davinci_003_outputs.json
 
 
 # TruthfulQA
 wget -P data/eval/truthfulqa https://github.com/sylinrl/TruthfulQA/raw/main/TruthfulQA.csv
 
 
-# Toxigen data
-mkdir -p data/eval/toxigen
-for minority_group in asian black chinese jewish latino lgbtq mental_disability mexican middle_east muslim native_american physical_disability trans women
-do
-    wget -O data/eval/toxigen/hate_${minority_group}.txt https://raw.githubusercontent.com/microsoft/TOXIGEN/main/prompts/hate_${minority_group}_1k.txt
-done
+# # Toxigen data
+# mkdir -p data/eval/toxigen
+# for minority_group in asian black chinese jewish latino lgbtq mental_disability mexican middle_east muslim native_american physical_disability trans women
+# do
+#     wget -O data/eval/toxigen/hate_${minority_group}.txt https://raw.githubusercontent.com/microsoft/TOXIGEN/main/prompts/hate_${minority_group}_1k.txt
+# done
 
 
-# IFEVAL data
-wget -P data/eval/ifeval https://github.com/google-research/google-research/raw/master/instruction_following_eval/data/input_data.jsonl
+# # IFEVAL data
+# wget -P data/eval/ifeval https://github.com/google-research/google-research/raw/master/instruction_following_eval/data/input_data.jsonl
 
 
-# XSTest data
-wget -P data/eval/xstest https://github.com/paul-rottger/exaggerated-safety/raw/main/xstest_v2_prompts.csv
+# # XSTest data
+# wget -P data/eval/xstest https://github.com/paul-rottger/exaggerated-safety/raw/main/xstest_v2_prompts.csv
 
 
-# we use self-instruct test set, and vicuna test set for our human evaluation
-mkdir -p data/eval/creative_tasks 
-wget -O data/eval/creative_tasks/self_instruct_test.jsonl https://github.com/yizhongw/self-instruct/raw/main/human_eval/user_oriented_instructions.jsonl
-wget -O data/eval/creative_tasks/vicuna_test.jsonl https://raw.githubusercontent.com/lm-sys/FastChat/main/fastchat/llm_judge/data/vicuna_bench/question.jsonl
+# # we use self-instruct test set, and vicuna test set for our human evaluation
+# mkdir -p data/eval/creative_tasks 
+# wget -O data/eval/creative_tasks/self_instruct_test.jsonl https://github.com/yizhongw/self-instruct/raw/main/human_eval/user_oriented_instructions.jsonl
+# wget -O data/eval/creative_tasks/vicuna_test.jsonl https://raw.githubusercontent.com/lm-sys/FastChat/main/fastchat/llm_judge/data/vicuna_bench/question.jsonl
